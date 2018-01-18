6895ddb2b387b8c2cb32e59dbfd5d22c6a02b005
alagappanpl <31503558+alagappanpl@users.noreply.github.com>
2018-01-18 11:02:01 +0000

    Create prefix.c

---
+++ prefix.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+ size_t maxlen = 0, len;
+ int i;
+ int longest;
+ for (i = 1; i < argc; i++) 
+  {
+  len = strlen(argv[i]);
+  if (len > maxlen) 
+  longest = argv[i];
+ }
+printf("longest string is %s. \n", longest);
+return 0;
+}
